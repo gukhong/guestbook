ab9e5cd4e5126101d9efcf4a8936ce364fcfcce8
hong <gukhong@gmail.com>
2024-08-20 23:25:45 +0900

    개발환경 초기설정

---
+++ src/test/java/com/gukhong/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gukhong.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
