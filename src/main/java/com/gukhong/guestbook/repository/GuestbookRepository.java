37731d2e51f429874db5ce4419a05f7de80cf017
hong <gukhong@gmail.com>
2024-08-20 23:57:59 +0900

    BaseEntity설정

---
+++ src/main/java/com/gukhong/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,7 @@
+package com.gukhong.guestbook.repository;
+
+import com.gukhong.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+}
