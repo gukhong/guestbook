b62933854d53a71571babcdcbb2edb6e3f6dc28e
hong <gukhong@gmail.com>
2024-09-01 02:15:49 +0900

    게시글 등록 기능 구현

--- src/main/java/com/gukhong/guestbook/controller/GuestbookController.java
+++ src/main/java/com/gukhong/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.gukhong.guestbook.controller;
 
+import com.gukhong.guestbook.dto.GuestbookDTO;
 import org.springframework.ui.Model;
 import com.gukhong.guestbook.dto.PageRequestDTO;
 import com.gukhong.guestbook.service.GuestbookService;
@@ -7,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -28,5 +31,22 @@ public class GuestbookController {
         log.info("list............."+ pageRequestDTO);
 
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+
+        log.info("dto..." + dto);
+
+        // 새로 추가된 엔티티 번호
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg",gno);
+        return "redirect:/guestbook/list";
     }
 }
--- src/main/java/com/gukhong/guestbook/service/GuestbookService.java
+++ src/main/java/com/gukhong/guestbook/service/GuestbookService.java
@@ -32,4 +32,5 @@ public interface GuestbookService {
 
                 return dto;
     }
+
 }
