de91b6e72b167327483069745db396758aeb12e7
hong <gukhong@gmail.com>
2024-09-01 01:54:01 +0900

    페이징처리

--- src/main/java/com/gukhong/guestbook/controller/GuestbookController.java
+++ src/main/java/com/gukhong/guestbook/controller/GuestbookController.java
@@ -1,5 +1,9 @@
 package com.gukhong.guestbook.controller;
 
+import org.springframework.ui.Model;
+import com.gukhong.guestbook.dto.PageRequestDTO;
+import com.gukhong.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor // 자동 주입을 위한 Annotation
 public class GuestbookController {
 
-    @GetMapping({"/","/list"})
-    public String list(){
-        log.info("list.............");
+    private final GuestbookService service; // final 선언
 
-        return "/guestbook/list";
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping({"/list"})
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("list............."+ pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
